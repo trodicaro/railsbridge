37e28898868effa4d8b11bbd70b2011b58b2a698
trodicaro <trodicaro@gmail.com>
2015-03-30 11:50:26 -0500

    updated the Heroku link

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Suggestotron::Application.routes.draw do
+resources :topics do
+  member do
+    post 'upvote'
+    post 'downvote'
+  end
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'topics#index'
+  get 'about' => 'pages#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
